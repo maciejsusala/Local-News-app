402161230ce8b55709339947c138c5cb6a38f846
maciejsusala <150375661+maciejsusala@users.noreply.github.com>
2024-09-11 16:16:13 +0200

    Created service to Import data from csv file to database.

--- src/main/java/com/maciejsusala/task_inksolutions/service/impl/CityServiceImpl.java
+++ src/main/java/com/maciejsusala/task_inksolutions/service/impl/CityServiceImpl.java
@@ -12,7 +12,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
