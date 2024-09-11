e613a671a6685563663a6ccd03e287e609bb5c60
maciejsusala <150375661+maciejsusala@users.noreply.github.com>
2024-09-11 11:58:34 +0200

    initial commit

---
+++ src/main/java/com/maciejsusala/task_inksolutions/TaskInksolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.maciejsusala.task_inksolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskInksolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskInksolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maciejsusala/task_inksolutions/TaskInksolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maciejsusala.task_inksolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskInksolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
